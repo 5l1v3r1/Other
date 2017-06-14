2ad6c0f0fa38f2e2da68fbcca6e51b294f66dd06
Michael Gottburg <mikagott@hotmail.com>
2017-06-14 09:59:34 +0200

    Created Website Index Base

---
+++ raspberry/html/index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+?>
+
+<!DOCTYPE HTML>
+  <html lang="en">
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta charset="UTF-8">
+  
+      <title>Office Plants Live</title>
+
+      <!-- Metadata` -->
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <meta name="author" content="Michael Gottburg">
+      <meta name="description" content="RedHat Neuchâtel Office Plants Video Live Feed">
+
+      <!-- CSS Stylesheet(s)` -->
+      <link href="css/main.css" rel="stylesheet">
+
+      <!-- Javascript` -->
+      <script src="js/jquery.js" type="text/javascript"></script>
+  
+      </head>
+      <body>
+
+      </body>
+  </html>
+<!-- End -->
