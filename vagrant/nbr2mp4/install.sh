e958acc74ee18076e37fa1e379dbd98d7b31b042
gottburgm <mikagott@hotmail.com>
2017-08-29 11:34:34 +0200

    - Renamed Vagrant Directory - Added nbr2mp4 Depencies Installation

--- vagrant/nbr2mp4/install.sh
+++ vagrant/nbr2mp4/install.sh
@@ -3,6 +3,9 @@
 echo "[+] DOwnloading Vagrant ..."
 wget https://releases.hashicorp.com/vagrant/1.9.8/vagrant_1.9.8_x86_64.deb -O vagrant.deb
 
+echo "[+] Installing Dependencies ..."
+sudo dnf install -y VirtualBox-\* vagrant-\* webkitgtk rhythmbox rhythmbox-\* empathy rubygem-nokogiri rubygem-factory_girl yelp yelp\* zenity
+
 echo "[+] Installing Package ..."
 sudo dpkg -i vagrant.deb
 
